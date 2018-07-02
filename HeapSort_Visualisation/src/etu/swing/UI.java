70a74462f10749d79b4228ce6fb5fda1185550ff
Segey <sergeyvladimirovish@gmail.com>
2018-07-02 17:12:06 +0300

    new project with all components

---
+++ HeapSort_Visualisation/src/etu/swing/UI.java
@@ -0,0 +1,63 @@
+package etu.swing;
+
+import etu.controller.Controller;
+import etu.heapSort.FileReadArray;
+import etu.heapSort.HeapSort;
+
+import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class UI extends JFrame{
+
+    private final ButtonsPanel buttons;
+    private final Component canvas;
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(()->new UI().setVisible(true));
+    }
+
+    private  UI() throws HeadlessException {
+        JPanel rootPanel = new JPanel(){
+            @Override
+            public void paint(Graphics g){
+                g.setColor(Color.cyan);
+                g.fillRect(5, 5, 700, 700); //size of inside window
+            }
+        };
+
+        buttons = new ButtonsPanel();
+        canvas = new JPanel();
+
+        canvas.setPreferredSize(new Dimension(900, 750));
+        buttons.setPreferredSize(new Dimension(900, 400));
+
+        rootPanel.setLayout(new BoxLayout(rootPanel, BoxLayout.Y_AXIS)); //WARNINNNGGGGGGG!
+        rootPanel.setPreferredSize( new Dimension(950, 750)); // size of MainWindow
+
+        rootPanel.add(buttons, BorderLayout.SOUTH);
+        rootPanel.add(canvas, BorderLayout.CENTER);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); //close app when close main window
+        setContentPane(rootPanel);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    private void initListners() throws FileNotFoundException {
+        //TODO Scaner blabla
+        Scanner in = new Scanner(new File("C:/Users/Sergey/IdeaProjects/HeapSort_Visualisation/resource/input.dat"));
+        FileReadArray f = FileReadArray.init(in);
+
+        HeapSort ob = new HeapSort();
+        ob.sort(f.getArray());
+
+        Controller = new Controller(ob);
+
+//        Timer timer = new Timer(50, e -> { controller.viewUpdated(); canvas.requestFocus(); });
+//        timer.setRepeats(true);
+//        timer.start();
+    }
+}
