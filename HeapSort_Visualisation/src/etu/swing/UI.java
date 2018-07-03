8540e18eae191e2a453f3e7fd8832d5637f93304
Segey <sergeyvladimirovish@gmail.com>
2018-07-03 20:25:09 +0300

    error

--- HeapSort_Visualisation/src/etu/swing/UI.java
+++ HeapSort_Visualisation/src/etu/swing/UI.java
@@ -64,7 +64,7 @@ public class UI extends JFrame{
 
         Controller controller = new Controller(heapSort, view, file);
 
-        buttons.addStartSort());
+        buttons.addStartSort(e -> controller.StartSort())); //ERROR HERE
 
         canvas.addKeyListener( new KeyAdapter() {
                @Override
