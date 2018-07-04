d4ea74f273311fc15fcaea3bb083eb4271b8b846
Cherkasova Elena <cherkasova.e@readyforsky.com>
2018-07-04 15:06:22 +0300

    cleaning

--- HeapSort_Visualisation/src/view/Graphics.java
+++
@@ -1,7 +0,0 @@
-package src.view;
-
-public interface Graphics {
-    void drawRect(int x, int y, int width, int height, int data);
-    void drawLine(int x1, int y1, int x2, int y2);
-    void showThanks();
-}
