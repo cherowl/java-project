f76cabd55186a0443f39769d52e36142e9fa92d1
Elena Cherkasova <cherowl@yandex.ru>
2018-07-06 09:45:29 +0300

    hp working on tree

--- HeapSort/src/etu/heapSort/HeapSort.java
+++ HeapSort/src/etu/heapSort/HeapSort.java
@@ -1,88 +1,96 @@
 package etu.heapSort;
 
+//import java.sql.SQLOutput;
 import java.util.Objects;
 
-public class HeapSort{
-
-    class BinTree {
-        public BinTree left;            // левое и правое поддеревья и ключ
-        public BinTree right;
-        public int key;
-//        private staint count = 0;
+class BinTree{
+    public BinTree left;            // левое и правое поддеревья и ключ
+    public BinTree right;
+    public int key;
 
 
-        public BinTree(int i) { // конструктор с инициализацией ключа
-            key = i;
+
+    public void create(int[] arr){
+        int i = 1;
+        while (!Objects.equals(i, arr.length)){
+            insert(new BinTree(arr[i++]));
         }
+    }
 
-        public void create(int[] arr) {  // корень бин.дер. уже существет - достраиваем остальное
-            int i = 1;
-            while (!Objects.equals(i, arr.length- 1)) {
-                insert(new BinTree(arr[i++]));
-            }
-        }
+    public BinTree(int i){ // конструктор с инициализацией ключа
+        key = i;
+    }
 
-        /*
-        insert (добавление нового поддерева (ключа))
+    /*  insert (добавление нового поддерева (ключа))
         сравнить ключ добавляемого поддерева (К) с ключом корневого узла (X).
         Если K>=X, рекурсивно добавить новое дерево в правое поддерево.
         Если K<X, рекурсивно добавить новое дерево в левое поддерево.
         Если поддерева нет, то вставить на это место новое дерево
-        */
-        public void insert(BinTree bt) {
-            if (bt.key < key)
-                if (left != null) left.insert(bt);
-                else left = bt;
-            else if (right != null) right.insert(bt);
-            else right = bt;
-        }
-
-        /*
-        visit (обход)
-		Рекурсивно обойти левое поддерево.
-		Применить функцию "sout" (печать) к корневому узлу.
-		Рекурсивно обойти правое поддерево.
-	    */
-        public void visit(BinTree node) {
-            if (left != null)
-                left.sortProgress(node);
-
-            node.sortProgress(this);
-
-            if (right != null)
-                right.sortProgress(node);
-        }
-
-        public void sortProgress(BinTree node) {
-//            for (int i = 0; i < count; i++) {
-//                System.out.print(" ");
-//            }
-
-//            getSortedArray()[count] = node.key;
-            System.out.println(node.key);
-
-        }
+    */
+    public void insert(BinTree bt){
+        if (bt.key < key)
+            if (left != null) left.insert(bt);
+            else left = bt;
+        else if (right != null) right.insert(bt);
+        else right = bt;
     }
-        public int arr[];
-        public int arrSorted[];
+    /*  visit (обход)
+        Рекурсивно обойти левое поддерево.
+        Применить функцию f (печать) к корневому узлу.
+        Рекурсивно обойти правое поддерево.
+    */
+    public void traverse(VisitorBT visitor){
+        if (left != null)
+            left.traverse(visitor);
 
-        public HeapSort(int[] arr){
-            this.arr = arr;
-            this.arrSorted = arr.clone();
-            BinTree bt = new BinTree(arr[0]);
-            bt.create(arr);
-            bt.visit(bt);
+		visitor.visit(this);
 
-        }
-
-        public int[] getSortedArray(){
-            return  arrSorted;
-        }
-
-
-
+        if (right != null)
+            right.traverse(visitor);
+    }
 }
 
+interface VisitorBT{
+    void visit(BinTree node);
+}
 
+class SortProcess implements VisitorBT {
+    private static int instanceCounter = 0;
+    private int counter;
+    private int arr[];
+
+    SortProcess(int[] arr) {
+        this.arr = arr;
+    }
+
+    public void visit(BinTree node){
+        counter = instanceCounter++;
+        printArr(arr);
+        arr[counter] = node.key;
+
+    }
+
+    private void printArr(int[] arr) {
+        for (Integer i : arr){
+            System.out.print(" "+i.intValue());
+        }
+        System.out.println();
+    }
+}
+
+public class HeapSort{
+    public int arr[];
+
+    public HeapSort(int[] arr){
+        this.arr = arr;
+        BinTree bt = new BinTree(arr[0]);
+        bt.create(arr);
+        bt.traverse(new SortProcess(arr));
+    }
+
+    public int[] getSortedArray(){
+        return  arr;
+    }
+}
 
 
