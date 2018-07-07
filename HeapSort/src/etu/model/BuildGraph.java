2c8abb27df99a746068b3e6b2dfe6c92b431675a
Segey <sergeyvladimirovish@gmail.com>
2018-07-07 17:04:37 +0300

    add KLP

--- HeapSort/src/etu/model/BuildGraph.java
+++ HeapSort/src/etu/model/BuildGraph.java
@@ -1,20 +1,35 @@
 package etu.model;
 
+import etu.model.HeapSort.BinTree;
 import org.graphstream.graph.Graph;
 import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.SingleGraph;
 
 public class BuildGraph {
 
-	private final int[] metaData;
+	private final BinTree binTree;
 
-	public BuildGraph(int[]            metaData) {
-		this.metaData = metaData;
+	public BuildGraph (BinTree binTree) {
+		this.binTree = binTree;
 	}
 
-//	public createNode(){
-//		Bin
-//	}
+	public void KLP(BinTree bt, int d, int offset, int i){
+
+		Node w = graph.addNode(String.valueOf(i));
+		w.setAttribute("xy", d, -bt.getLevel());
+		w.setAttribute("label", bt.getKey());
+
+		if (bt.getLeft() != null) {
+			KLP(bt.getLeft(), - d - offset, offset/2, i++ );
+		}
+
+		if (right != null)
+			KLP(bt.getRight(), d + offset, offset/2, i++ );
+	}
+
+	public void createNode(BinTree binTree){
+		
+	}
 
 	public Graph build(){
 		Graph graph = new SingleGraph("G", false, true);
@@ -38,42 +53,42 @@ public class BuildGraph {
 						"size-mode: dyn-size;}");
 
 //		int d = 4;
-		int offset = 4;
-		int axis_x = 0;
-		int level = 0, count_el_on_vlv = 0;
-		for (int i = 1; i < metaData.length-1; i++) {
-			count_el_on_vlv = (int)Math.pow(2, i);
-
-			if( i == count_el_on_vlv && i!=0 ) {
-				offset = (int)Math.pow(-1,i)*offset;
-				axis_x += offset;
-				offset /= 2;
-				level++;
-				System.out.println("level++");
-			}
-			Node w = graph.addNode(String.valueOf(i));
-			w.setAttribute("xy", axis_x, -level);
-
-//			if(i == 0) {
-//				w.setAttribute("xy", 0, 0);
-//				level++;
-//			}else if( i == 1 || i == 2){
-//				w.setAttribute("xy", Math.pow(-1, i) * d, -level);
-//				offset /=2;
-//				level++;
-//			}else {
-//				w.setAttribute("xy", level * d + Math.pow(-1, i+1) * offset, -level);
+//		int offset = 4;
+//		int axis_x = 0;
+//		int level = 0, count_el_on_vlv = 0;
+//		for (int i = 1; i < metaData.length-1; i++) {
+//			count_el_on_vlv = (int)Math.pow(2, i);
+//
+//			if( i == count_el_on_vlv && i!=0 ) {
+//				offset = (int)Math.pow(-1,i)*offset;
+//				axis_x += offset;
 //				offset /= 2;
+//				level++;
+//				System.out.println("level++");
 //			}
-			w.setAttribute("label", metaData[i]);
-		}
-
-		for (int i = 1, k = 0; i < metaData.length; i++) {
-			graph.addEdge(String.valueOf(i), String.valueOf(k), String.valueOf(i), true);
-			if(i % 2 == 0) {
-				k++;
-			}
-		}
+//			Node w = graph.addNode(String.valueOf(i));
+//			w.setAttribute("xy", axis_x, -level);
+//
+////			if(i == 0) {
+////				w.setAttribute("xy", 0, 0);
+////				level++;
+////			}else if( i == 1 || i == 2){
+////				w.setAttribute("xy", Math.pow(-1, i) * d, -level);
+////				offset /=2;
+////				level++;
+////			}else {
+////				w.setAttribute("xy", level * d + Math.pow(-1, i+1) * offset, -level);
+////				offset /= 2;
+////			}
+//			w.setAttribute("label", metaData[i]);
+//		}
+//
+//		for (int i = 1, k = 0; i < metaData.length; i++) {
+//			graph.addEdge(String.valueOf(i), String.valueOf(k), String.valueOf(i), true);
+//			if(i % 2 == 0) {
+//				k++;
+//			}
+//		}
 
 
 		return graph;
