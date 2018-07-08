df46dd6feb83d23566891efc976a55075356b2d0
Elena Cherkasova <cherowl@yandex.ru>
2018-07-08 13:03:57 +0300

    button can  working

--- HeapSort/src/etu/controller/Controller.java
+++ HeapSort/src/etu/controller/Controller.java
@@ -1,22 +1,27 @@
 package etu.controller;
 
+import etu.model.FileReadArray;
+import etu.model.heapsort.HeapSort;
+import etu.view.View;
+
 public class Controller {
-//    private final HeapSort heapSort;
-//    private final FileReadArray readFile;
-//    private final View view;
-//
-//    public Controller(HeapSort heapSort, View view, FileReadArray readFile) {
-//        this.heapSort = heapSort;
-//        this.view = view;
-//        this.readFile = readFile;
-//    }
-//
+    private final HeapSort heapSort;
+    private final FileReadArray readFile;
+    private final View view;
+
+    public Controller(HeapSort heapSort, View view, FileReadArray readFile) {
+        this.heapSort = heapSort;
+        this.view = view;
+        this.readFile = readFile;
+    }
+
 //    public void viewUpdated(){
 //        view.draw(heapSort);
 //    }
-//
-//    public void StartSort(){
+
+    public void StartSort(){
 //        heapSort.sort(readFile.getArray());
 //        view.draw(heapSort);
-//    }
+        System.out.println("testttttt");
+    }
 }
